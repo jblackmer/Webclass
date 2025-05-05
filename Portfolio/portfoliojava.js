c48f81597adc61447d1c3ce87b10ddae83c61faa
jblackmer <158094004+jblackmer@users.noreply.github.com>
2025-05-05 12:06:16 -0400

    Add files via upload

---
+++ Portfolio/portfoliojava.js
@@ -0,0 +1,7 @@
+function openBook(bookId) {
+    document.getElementById(bookId).style.display = "block";
+}
+
+function closeBook(bookId) {
+    document.getElementById(bookId).style.display = "none";
+}
